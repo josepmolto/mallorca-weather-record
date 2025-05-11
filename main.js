8a96aaafa92904fa0229d5c4a26ccc87366ee3eb
Josep Molto <josep.molto@gmail.com>
2025-05-11 18:47:57 +0200

    Add Github Action

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
 const axios = require("axios");
-const fs = require("fs/promises");
+const fs = require("fs");
 const path = require("path");
 const stations = require("./stations.json");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const DATE = getYesterdayDate();
 const URL =
   "https://api.weather.com/v2/pws/dailysummary/3day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&stationId=VALUE&numericPrecision=decimal&format=json&units=m";
-const FOLDER_NAME = "./data";
+const FOLDER_DATA = createAndGetFolderData();
 
-async function downloadAndSave(stationId, townName) {
+async function downloadStationSummaryData(stationId, townName) {
   try {
-    await fs.mkdir(FOLDER_NAME, { recursive: true });
-
     const url = URL.replace("VALUE", stationId);
     const response = await axios.get(url);
-    const fullPath = path.join(FOLDER_NAME, `${townName}.json`);
-
-    // await fs.writeFile(
-    //   fullPath,
-    //   JSON.stringify(response.data, null, 2),
-    //   "utf8"
-    // );
 
     return response.data;
-    console.log(`✅ File saved to ${fullPath}`);
   } catch (error) {
-    console.error("❌ Error fetching or saving data:", error.message);
+    console.error("❌ Error fetching data:", error.message);
   }
 }
 
@@ -42,6 +32,19 @@ function getYesterdayDate() {
   function padDate(num) {
     return num.toString().padStart(2, 0);
   }
+}
+
+function createAndGetFolderData() {
+  const dateSplitted = DATE.split("-");
+  const folderPath = path.join(
+    "./data",
+    dateSplitted[0],
+    dateSplitted[1],
+    dateSplitted[2]
+  );
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  return folderPath;
 }
 
 function getDefaultResponse(townName) {
@@ -81,20 +84,6 @@ function mapStationData(townName, stationData) {
   return getDefaultResponse(townName);
 }
 
-const csvWriter = createCsvWriter({
-  path: path.join(__dirname, "data", "station-summary.csv"),
-  header: [
-    { id: "station", title: "Station" },
-    { id: "date", title: "Date" },
-    { id: "maxTemperature", title: "Max Temperature (°C)" },
-    { id: "minTemperature", title: "Min Temperature (°C)" },
-    { id: "maxHumidity", title: "Max Humidity (%)" },
-    { id: "minHumidity", title: "Min Humidity (%)" },
-    { id: "maxWind", title: "Max Wind (km/h)" },
-    { id: "totalPrecipitation", title: "Total Precipitation (l)" },
-  ],
-});
-
 async function main() {
   console.log(`[*] Extracting summary at ${DATE}`);
   const stationsData = [];
@@ -106,12 +95,28 @@ async function main() {
       continue;
     }
 
-    const stationData = await downloadAndSave(id, townName.toUpperCase());
+    const stationData = await downloadStationSummaryData(
+      id,
+      townName.toUpperCase()
+    );
     const mappedData = mapStationData(townName, stationData);
 
     stationsData.push(mappedData);
   }
 
+  const csvWriter = createCsvWriter({
+    path: path.join(FOLDER_DATA, "station-summary.csv"),
+    header: [
+      { id: "station", title: "Station" },
+      { id: "date", title: "Date" },
+      { id: "maxTemperature", title: "Max Temperature (°C)" },
+      { id: "minTemperature", title: "Min Temperature (°C)" },
+      { id: "maxHumidity", title: "Max Humidity (%)" },
+      { id: "minHumidity", title: "Min Humidity (%)" },
+      { id: "maxWind", title: "Max Wind (km/h)" },
+      { id: "totalPrecipitation", title: "Total Precipitation (l)" },
+    ],
+  });
   csvWriter
     .writeRecords(stationsData)
     .then(() => console.log("✅ CSV file written successfully."))
